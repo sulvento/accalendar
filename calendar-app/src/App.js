0a930c3fa1e5738697257d06be08f317027b6b84
ajlgg <luthraa223@gmail.com>
2026-03-05 20:21:36 -0500

    Welcome to Accalendar text

--- calendar-app/src/App.js
+++ calendar-app/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 
 function App() {
-  return <div></div>;
+  return (
+  <div className="flex h-screen items-center justify-center"> 
+    <h1 className="text-[50px] text-red-500 text-bold">Welcome to Accalendar</h1>
+  </div>
+  );
 }
 
 export default App;
