e83ab8eaf61921b60ae23e534995575a4ceb711a
ajlgg <luthraa223@gmail.com>
2026-04-25 05:14:58 -0400

    basic landing page

--- calendar-app/src/screens/Welcome.js
+++ calendar-app/src/screens/Welcome.js
@@ -4,28 +4,24 @@ import Button from "../Components/Btn/Button"
 import NavBar from "../Components/navb/NavBar"
 
 
-
-
 function Welcome(){
     const navigate = useNavigate();
 
     return (
-        <div>
-            <NavBar className="m-1 rounded flex w-screen "></NavBar>
-            <div className="flex flex-col h-screen items-center justify-center gap-6"> 
-                <h1 className="text-[60px] text-red-500 text-bold border border-yellow-400 rounded-lg">Increase Your Productivtity</h1>
-                <div>
-                    <Button onClick={() => navigate("/calendar")}>Enter</Button>
-                </div>
-                <div>
-                
-                </div>
+        <div className="min-h-screen w-full overflow-x-hidden">
+            <NavBar src="images/accalendar.png" className="w-full rounded" />
+
+            <div className="flex flex-col items-center justify-center gap-6 min-h-screen">
+                <h1 className="text-[60px] text-red-500 font-bold border border-yellow-400 rounded-lg">
+                    Increase Your Productivity
+                </h1>
+
+                <Button onClick={() => navigate("/calendar")}>
+                    Enter
+                </Button>
             </div>
         </div>
-       
-        
     );
-    
 }
 
 
